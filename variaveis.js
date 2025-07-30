eb1ee7574c302b9c49bd919aa2ff8e9945cdd7dd
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2025-07-30 08:41:28 -0300

    adiciona variáveis de personagem e mensagem de boas-vindas

---
+++ variaveis.js
@@ -0,0 +1,9 @@
+let nome = "Artemis";
+let classe = "arqueira";
+let nivel = 27;
+let energia = 100;
+let moedas = 50;
+
+console.log(`Olá! Eu sou ${nome}, uma ${classe} nível ${nivel} com ${energia} de energia e ${moedas} moedas.`);
+
+
